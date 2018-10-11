a0ab195079377cf28504bf59a179ad8340a46a62
Tushar <gupta.tus@husky.neu.edu>
2018-10-11 13:41:40 -0400

    added chat box working

--- chat-application/src/components/MessagesList.js
+++ chat-application/src/components/MessagesList.js
@@ -1,29 +1,28 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import Message from './Message';
+import React from 'react'
+import PropTypes from 'prop-types'
+import Message from './Message'
 
-
-const MessagesList = ({message, author}) => (
-    <section id="message-list">
-        <ul>
-            {message.map(message => (
-                <Message key={message.id}
-                    {...message} />
-            ))}
-        </ul>
-
-    </section>
+const MessagesList = ({ messages }) => (
+	<section id="messages-list">
+		<ul>
+			{messages.map(message => (
+				<Message
+					key={message.id}
+					{...message}
+				/>
+			))}
+		</ul>
+	</section>
 )
-    
 
 MessagesList.PropTypes = {
-    messages: PropTypes.arrayOf(
-        PropTypes.shape({
-            id: PropTypes.number.isRequired,
-            message: PropTypes.string.isRequired,
-            author: PropTypes.string.isRequired,
-        }).isRequired
-    ).isRequired
+	messages: PropTypes.arrayOf(
+		PropTypes.shape({
+			id: PropTypes.number.isRequired,
+			message: PropTypes.string.isRequired,
+			author: PropTypes.string.isRequired,
+		}).isRequired
+	).isRequired
 }
 
 export default MessagesList
--- chat-application/src/index.js
+++ chat-application/src/index.js
@@ -5,15 +5,21 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
+import { addUser } from './actions'
 
 import reducers from './reducers'
 
 const store = createStore(reducers)
 
+store.dispatch(addUser('Me'));
 
 
-
-ReactDOM.render(<Provider store={store}> <App /></Provider>, document.getElementById('root'));
+ReactDOM.render(
+	<Provider store={store}>
+		<App />
+	</Provider>, 
+	document.getElementById('root')
+	);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
